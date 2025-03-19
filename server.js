51e6bfb3fb354273495cfb55fb7d420e02fc7e26
Kirill <stoporenko.k@gmail.com>
2025-03-20 06:13:19 +0700

    added mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const dotenv = require("dotenv").config();
-const {errorHandler} = require("./middleware/errorMiddleware");
+const colors = require('colors');
 const port = process.env.PORT || 5000;
+
+const {errorHandler} = require("./middleware/errorMiddleware");
+
+const connectDB = require("./config/db");
+connectDB();
 
 const express = require("express");
 
