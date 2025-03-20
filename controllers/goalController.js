93a99b8b07eba364a4b03772575f7669ddf091a6
Kirill <stoporenko.k@gmail.com>
2025-03-20 07:06:05 +0700

    made update goal method

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -35,7 +35,16 @@ const getGoals = asyncHandler(async (req, res) => {
 // @access Pravate
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    res.status(200).json(updatedGoal);
 });
 
 
